bbd8e2d2303d5db653775add0763a58c51de6d05
Cees Paris <ceesparis@wcw-stud-145-109-41-128.wireless.uva.nl>
2021-12-08 16:39:48 +0100

    thirdday

--- search_engine/src/App.js
+++ search_engine/src/App.js
@@ -1,37 +1,65 @@
 import { useEffect, useState } from 'react';
-import Xml_reader from './xml_reader'
+import data from './Database.json'
 
 function App() {
   const [search, setSearch] = useState('');
-  // const [results, setResults] = useState([]);
-  const [database, setDatabase] = useState('');
+  const [results, setResults] = useState([]);
+  const [database, setDatabase] = useState([]);
+  const [result_counts, setCount] = useState(0)
  
-  const loadWorks = async () => {
-    //  e.preventDefault()
-     await Xml_reader(setDatabase);
-     console.log(database);
-  }
+  // const loadWorks = () => { 
+  //   return new Promise((resolve, reject)=> {
+  //   setDatabase(data);
+  //     if(true){
+  //       resolve()
+  //     } else {
+  //       reject('error: something went wrong')
+  //     }
+  //   });
+  // }
 
   useEffect(() => {
-    loadWorks();
-    // console.log('render');
-  }, []);
+    // import('./Database.json').then(data => setDatabase(data));
+    setDatabase(data)
 
-  console.log(database)
+    // await loadWorks();
+    // alert('Database Loaded');
+    // console.log(database[0]);
+  }, [])
+
+  const searchDatabase = (search) => {
+    const searchword = Object.values({search});
+    var counter = 0;
+    var text_list = [];
+    // console.log(searchword);
+    for (var i in database) {
+      const entry = database[i]
+      const text = entry.text;
+      // console.log(text);
+      if (text.includes(searchword)){
+          console.log(text);
+          counter += 1;
+          text_list.push(text)
+      }
+    }
+    setCount(counter);
+    setResults(text_list);
+  }
 
   const handleSearch = async (e) => {
     e.preventDefault()
     if (search === ''){
       return;
-    }
-    else {
-      // const test = Xml_reader(setDatabase);
-      // console.log(test)
-      
-    }
-
+    } else {
+        searchDatabase(search);
+      }
   }
 
+  if (database.length === 0) {
+    return (
+      <div>loading...</div>
+    )
+  }
 
   return (
     <div className='Searchengine'>
@@ -44,10 +72,13 @@ function App() {
              onChange={e => setSearch(e.target.value)}
              />
         </form>
-        <p>Search Results: 0</p>
+        <p>Search Results: {result_counts}</p>
       </header>
       <div className='Results'>
-          <div className='Result'>
+        {
+          results.map((result, i) => {
+            return (
+              <div className='Result'>
             <span className='result_title'>title goes here</span>
             <span className='result_date'>date goes here</span>
             <div className='Result_text'>
@@ -58,6 +89,9 @@ function App() {
               <a href='#'>expand</a>
             </div>
           </div>
+            )
+          })
+        }
       </div>
     </div>
   );
